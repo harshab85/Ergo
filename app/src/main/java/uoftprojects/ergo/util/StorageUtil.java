5b0a8a7e1b9661a229aadc5d2f0b9c54b46df50a
harshab85 <harshab85@gmail.com>
2015-03-10 15:18:24 -0400

    Util

---
+++ app/src/main/java/uoftprojects/ergo/util/StorageUtil.java
@@ -0,0 +1,21 @@
+package uoftprojects.ergo.util;
+
+import android.content.SharedPreferences;
+
+/**
+ * Created by H on 3/9/2015.
+ */
+public final class StorageUtil {
+
+    private static final SharedPreferences sharedPreferences = ActivityUtil.getMainActivity().getSharedPreferences("ErgoSetup", 0);
+    private static final SharedPreferences.Editor editor = sharedPreferences.edit();
+
+    public static boolean addLocalFlag(String flagName, boolean value){
+        editor.putBoolean(flagName, value);
+        return editor.commit();
+    }
+
+    public static boolean getFlag(String flagName){
+        return sharedPreferences.getBoolean(flagName, false);
+    }
+}

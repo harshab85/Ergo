09bae21a187329c1a9f72359d35b51cbc5ad54da
harshab85 <harshab85@gmail.com>
2015-03-25 04:11:38 -0400

    Partial seekbar changes

--- app/src/main/java/uoftprojects/ergo/alerts/handlers/TiltHandler.java
+++ app/src/main/java/uoftprojects/ergo/alerts/handlers/TiltHandler.java
@@ -2,8 +2,12 @@ package uoftprojects.ergo.alerts.handlers;
 
 import android.content.Context;
 import android.os.Vibrator;
+import android.view.View;
+import android.widget.SeekBar;
 import android.widget.Toast;
+import android.widget.VideoView;
 
+import uoftprojects.ergo.R;
 import uoftprojects.ergo.alerts.handlers.baseline.Baseline;
 import uoftprojects.ergo.metrics.IMetric;
 import uoftprojects.ergo.metrics.Tilt;
@@ -18,6 +22,8 @@ public class TiltHandler implements IHandler {
     //private Vibrator vibrator = null;
 
     private static TiltHandler INSTANCE = null;
+    private SeekBar seekBar = (SeekBar)ActivityUtil.getMainActivity().findViewById(R.id.tilt_detection);
+    //private int videoSeekTime;
 
     private TiltHandler(){
         //vibrator = (Vibrator) ActivityUtil.getMainActivity().getSystemService(Context.VIBRATOR_SERVICE) ;
@@ -46,7 +52,7 @@ public class TiltHandler implements IHandler {
             return false;
         }
 
-        float tiltAngle = tilt.getValue();
+        final float tiltAngle = tilt.getValue();
         if(tiltAngle > Baseline.PHONE_FLAT_MAX_ANGLE) {
             if (tiltAngle < Baseline.MIN_TILT_ANGLE || tiltAngle > Baseline.MAX_TILT_ANGLE) {
 
@@ -54,14 +60,22 @@ public class TiltHandler implements IHandler {
                     @Override
                     public void run() {
                         VideoUtil.pauseVideo();
+
+                        /*VideoView view2 = (VideoView)ActivityUtil.getMainActivity().findViewById(R.id.videoViewMaterial);
+                        videoSeekTime = view2.getCurrentPosition();*/
+                        //view2.setVisibility(View.INVISIBLE);
+
+                        
+
+                        seekBar.setVisibility(View.VISIBLE);
+                        //seekBar.setEnabled(false);
+                        seekBar.bringToFront();
+                        seekBar.setProgress((int)tiltAngle);
+                        seekBar.setAlpha(1f);
+
+                        Toast.makeText(ActivityUtil.getMainActivity(), "Ideal tilt angle (40 to 70). Current : " + tiltAngle, Toast.LENGTH_SHORT).show();
                     }
                 });
-
-
-                String message = "Hold phone at the correct angle (40 to 70). Current: " + tiltAngle;
-
-                //vibrator.vibrate(Baseline.VIBRATION_ALERT_PATTERN, 0);
-                Toast.makeText(ActivityUtil.getMainActivity(), message, Toast.LENGTH_SHORT).show();
 
                 return true;
             }
@@ -78,13 +92,18 @@ public class TiltHandler implements IHandler {
 
     @Override
     public void cancel() {
-        /*if(vibrator != null){
-            vibrator.cancel();
-        }*/
-
         ActivityUtil.getMainActivity().runOnUiThread(new Runnable() {
             @Override
             public void run() {
+                seekBar.setVisibility(View.INVISIBLE);
+                /*VideoView view2 = (VideoView)ActivityUtil.getMainActivity().findViewById(R.id.videoViewMaterial);
+                view2.setVisibility(View.VISIBLE);*/
+
+                /*if(videoSeekTime > 0) {
+                    view2.seekTo(videoSeekTime);
+                    videoSeekTime = 0;
+                }*/
+
                 VideoUtil.resumeVideoWhenPaused();
             }
         });
